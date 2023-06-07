458e3375c346b4ffd26081655366d6dbc6500d56
Zack Gibbs <andros@pop-os.localdomain>
2023-06-07 12:39:19 -0400

    refresh

--- apps/playground/src/app/svgs/svgs.module.ts
+++ apps/playground/src/app/svgs/svgs.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SvgsComponent } from './svgs.component';
+import { BarComponent } from './bar/bar.component';
 
 
 @NgModule({
   declarations: [
     SvgsComponent,
+    BarComponent,
 
   ],
   imports: [
