4903ccc31563bab44aeb96a0c43ff7c9fe484c06
Zack Gibbs <zsgibbs@outlook.com>
2023-06-28 19:00:51 -0400

    wta dynamic reactive forms

--- apps/playground/src/app/designs/compinteract/compinteract.component.ts
+++ apps/playground/src/app/designs/compinteract/compinteract.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'gibbsltd-compinteract',
@@ -8,6 +9,8 @@ import { FormBuilder } from '@angular/forms';
 })
 export class CompinteractComponent implements OnInit {
 
+  unsubscribe$ = new Subject<void>();
+
   constructor(private _fg:FormBuilder,){}
 
   groupInput = this._fg.group({
@@ -15,33 +18,47 @@ export class CompinteractComponent implements OnInit {
     userName: 'andros',
     quantity: 5,
     targetTension: 120,
-    //actualTension:115,
+    actualTension:115,
 
   })
 
   getUserName(){ return this.groupInput.value.userName }
   getQuantity(){ return this.groupInput.value.quantity||3 }
   getTarget(){ return this.groupInput.value.targetTension||20 }
+  getActualTension() {return this.groupInput.value.actualTension||100}
+
+  baseline$=this.groupInput.statusChanges
+  .pipe(takeUntil(this.unsubscribe$))
+  .subscribe((x)=>{
+    this.initObjz()
+    console.clear()
+    this.showObj()
+  })
+
+  //Lifecycle Hooks
+
+  ngOnDestroy(){
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
 
   max=180; //px length for D3
 
-  objLeft:{count:number,dist:number, x:number; y:number}[]= []
+  objLeft :{count:number,dist:number, x:number; y:number}[]= []
   objRight:{count:number,dist:number, x:number; y:number}[]= []
 
   read2D3conv=(this.max*.95)/this.getTarget();
 
   initObjz(){
-
-
     this.objLeft=Array(this.getQuantity()).fill({})
-    .map((x,i)=>x={count:i,dist:this.getTarget()*this.read2D3conv,x:5,y:7});
+    .map((x,i)=>x={count:i,dist:this.getActualTension()*this.read2D3conv,x:5,y:7});
 
     this.objRight=Array(this.getQuantity()).fill({})
-    .map((x,i)=>x={count:i,dist:this.getTarget()*this.read2D3conv,x:5,y:7});
+    .map((x,i)=>x={count:i,dist:this.getActualTension()*this.read2D3conv,x:5,y:7});
    //console.log(this.objLeft);
+
   }
-
-
 
   showObj(){
    console.log(this.objLeft);
